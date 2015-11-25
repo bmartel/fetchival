6644399a3a89735ea930be956e2d292461275f32
bmartel <brandonmartel@gmail.com>
2015-11-24 20:29:28 -0600

    weird spacing

--- index.js
+++ index.js
@@ -32,7 +32,7 @@
     return fetchival.fetch(url, opts)
       .then(function (response) {
         if(response.status == 204) {
-					return {}
+	  return {}
         }
         if (response.status >= 200 && response.status < 300) {
           return response[opts.responseAs]()
